caab75df86e514109d72f26343782c108bda6dcb
amirulasri <amirulasrix@gmail.com>
2022-03-05 21:04:59 +0800

    Update Receipt Layout

--- src/main/Receipt.java
+++ src/main/Receipt.java
@@ -37,7 +37,7 @@ public class Receipt extends JFrame {
 		
 		setIconImage(Toolkit.getDefaultToolkit().getImage(Receipt.class.getResource("/main/logo/logo.png")));
 		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-		setBounds(100, 100, 936, 636);
+		setBounds(100, 100, 956, 673);
 		contentPane = new JPanel();
 		contentPane.setBackground(new Color(230, 184, 156));
 		setContentPane(contentPane);
@@ -52,54 +52,11 @@ public class Receipt extends JFrame {
 		lblNewLabel_4.setFont(new Font("SansSerif", Font.PLAIN, 18));
 		lblNewLabel_4.setForeground(Color.BLACK);
 		
-				JLabel lblNewLabel_1 = new JLabel("Name:");
-				lblNewLabel_1.setForeground(Color.BLACK);
-				lblNewLabel_1.setFont(new Font("SansSerif", Font.PLAIN, 16));
-		
-		JLabel namedisplay = new JLabel(customerdata.get(0).getname());
-		namedisplay.setForeground(Color.BLACK);
-		namedisplay.setFont(new Font("SansSerif", Font.PLAIN, 14));
-		
-				JLabel lblNewLabel_2 = new JLabel("Phone Number:");
-				lblNewLabel_2.setForeground(Color.BLACK);
-				lblNewLabel_2.setFont(new Font("SansSerif", Font.PLAIN, 16));
-		
-		JLabel phonenodisplay = new JLabel(customerdata.get(0).getphoneno());
-		phonenodisplay.setForeground(Color.BLACK);
-		phonenodisplay.setFont(new Font("SansSerif", Font.PLAIN, 14));
-		
 		JLabel lblNewLabel_4_2 = new JLabel("Order Details");
 		lblNewLabel_4_2.setHorizontalAlignment(SwingConstants.RIGHT);
 		lblNewLabel_4_2.setForeground(Color.BLACK);
 		lblNewLabel_4_2.setFont(new Font("SansSerif", Font.PLAIN, 18));
 		lblNewLabel_4_2.setBackground(Color.WHITE);
-		
-		JLabel lblNewLabel_1_2 = new JLabel("Order Date:");
-		lblNewLabel_1_2.setForeground(Color.BLACK);
-		lblNewLabel_1_2.setFont(new Font("SansSerif", Font.PLAIN, 16));
-		
-		JLabel orderdatedisplay = new JLabel(orderdata.get(0).getdate() + " " + orderdata.get(0).getordertime());
-		orderdatedisplay.setHorizontalAlignment(SwingConstants.RIGHT);
-		orderdatedisplay.setForeground(Color.BLACK);
-		orderdatedisplay.setFont(new Font("SansSerif", Font.PLAIN, 14));
-		
-		JLabel lblNewLabel_1_2_1 = new JLabel("Order ID:");
-		lblNewLabel_1_2_1.setForeground(Color.BLACK);
-		lblNewLabel_1_2_1.setFont(new Font("SansSerif", Font.PLAIN, 16));
-		
-		JLabel orderiddisplay = new JLabel(orderid);
-		orderiddisplay.setHorizontalAlignment(SwingConstants.RIGHT);
-		orderiddisplay.setForeground(Color.BLACK);
-		orderiddisplay.setFont(new Font("SansSerif", Font.PLAIN, 14));
-		
-		JLabel lblNewLabel_1_2_2 = new JLabel("Payment Type:");
-		lblNewLabel_1_2_2.setForeground(Color.BLACK);
-		lblNewLabel_1_2_2.setFont(new Font("SansSerif", Font.PLAIN, 16));
-		
-		JLabel paymenttypedisplay = new JLabel(paymentdata.get(0).getpaymenttype());
-		paymenttypedisplay.setHorizontalAlignment(SwingConstants.RIGHT);
-		paymenttypedisplay.setForeground(Color.BLACK);
-		paymenttypedisplay.setFont(new Font("SansSerif", Font.PLAIN, 14));
 		
 		JScrollPane scrollPane = new JScrollPane();
 		
@@ -114,23 +71,13 @@ public class Receipt extends JFrame {
 		JLabel balancedisp = new JLabel("Balance: RM" + priceformatter.format(paymentdata.get(0).getcustpay() - paymentdata.get(0).gettotalprice()));
 		balancedisp.setFont(new Font("Tahoma", Font.PLAIN, 14));
 		
-		JLabel lblNewLabel_2_1 = new JLabel("Address:");
-		lblNewLabel_2_1.setForeground(Color.BLACK);
-		lblNewLabel_2_1.setFont(new Font("SansSerif", Font.PLAIN, 16));
-		
 		String addressline = "<html>" + customerdata.get(0).getaddress().replaceAll("<","&lt;").replaceAll(">", "&gt;").replaceAll("\n", "<br/>") + "</html>";
-		JLabel addressdisplay = new JLabel(addressline);
-		addressdisplay.setVerticalAlignment(SwingConstants.TOP);
-		addressdisplay.setForeground(Color.BLACK);
-		addressdisplay.setFont(new Font("SansSerif", Font.PLAIN, 14));
 		
-		JLabel lblNewLabel_2_2 = new JLabel("Gender:");
-		lblNewLabel_2_2.setForeground(Color.BLACK);
-		lblNewLabel_2_2.setFont(new Font("SansSerif", Font.PLAIN, 16));
+		JPanel panel_1 = new JPanel();
+		panel_1.setOpaque(false);
 		
-		JLabel lblGendeDisp = new JLabel(customerdata.get(0).getgender());
-		lblGendeDisp.setForeground(Color.BLACK);
-		lblGendeDisp.setFont(new Font("SansSerif", Font.PLAIN, 14));
+		JPanel panel_2 = new JPanel();
+		panel_2.setOpaque(false);
 		GroupLayout gl_contentPane = new GroupLayout(contentPane);
 		gl_contentPane.setHorizontalGroup(
 			gl_contentPane.createParallelGroup(Alignment.LEADING)
@@ -152,42 +99,11 @@ public class Receipt extends JFrame {
 						.addComponent(balancedisp, Alignment.LEADING, GroupLayout.DEFAULT_SIZE, 598, Short.MAX_VALUE))
 					.addContainerGap(312, Short.MAX_VALUE))
 				.addGroup(gl_contentPane.createSequentialGroup()
-					.addContainerGap()
-					.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING)
-						.addGroup(gl_contentPane.createSequentialGroup()
-							.addComponent(lblNewLabel_2_2, GroupLayout.PREFERRED_SIZE, 108, GroupLayout.PREFERRED_SIZE)
-							.addPreferredGap(ComponentPlacement.UNRELATED)
-							.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING)
-								.addComponent(addressdisplay, GroupLayout.PREFERRED_SIZE, 465, GroupLayout.PREFERRED_SIZE)
-								.addComponent(lblGendeDisp, GroupLayout.PREFERRED_SIZE, 207, GroupLayout.PREFERRED_SIZE)))
-						.addGroup(gl_contentPane.createSequentialGroup()
-							.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING)
-								.addComponent(lblNewLabel_2)
-								.addComponent(lblNewLabel_1))
-							.addPreferredGap(ComponentPlacement.UNRELATED)
-							.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING)
-								.addComponent(namedisplay, GroupLayout.PREFERRED_SIZE, 333, GroupLayout.PREFERRED_SIZE)
-								.addComponent(phonenodisplay, GroupLayout.PREFERRED_SIZE, 207, GroupLayout.PREFERRED_SIZE))))
-					.addGap(16)
-					.addGroup(gl_contentPane.createParallelGroup(Alignment.TRAILING)
-						.addGroup(gl_contentPane.createSequentialGroup()
-							.addComponent(lblNewLabel_1_2)
-							.addPreferredGap(ComponentPlacement.UNRELATED)
-							.addComponent(orderdatedisplay, GroupLayout.PREFERRED_SIZE, 209, GroupLayout.PREFERRED_SIZE))
-						.addGroup(gl_contentPane.createSequentialGroup()
-							.addComponent(lblNewLabel_1_2_1, GroupLayout.PREFERRED_SIZE, 82, GroupLayout.PREFERRED_SIZE)
-							.addPreferredGap(ComponentPlacement.RELATED)
-							.addComponent(orderiddisplay, GroupLayout.PREFERRED_SIZE, 211, GroupLayout.PREFERRED_SIZE))
-						.addGroup(gl_contentPane.createSequentialGroup()
-							.addComponent(lblNewLabel_1_2_2, GroupLayout.PREFERRED_SIZE, 113, GroupLayout.PREFERRED_SIZE)
-							.addGap(90)
-							.addComponent(paymenttypedisplay, GroupLayout.PREFERRED_SIZE, 96, GroupLayout.PREFERRED_SIZE)))
+					.addComponent(panel_1, GroupLayout.PREFERRED_SIZE, 548, GroupLayout.PREFERRED_SIZE)
+					.addGap(10)
+					.addComponent(panel_2, GroupLayout.PREFERRED_SIZE, 362, GroupLayout.PREFERRED_SIZE)
 					.addContainerGap())
-				.addGroup(gl_contentPane.createSequentialGroup()
-					.addContainerGap()
-					.addComponent(lblNewLabel_2_1, GroupLayout.PREFERRED_SIZE, 73, GroupLayout.PREFERRED_SIZE)
-					.addContainerGap(837, Short.MAX_VALUE))
-				.addComponent(scrollPane, GroupLayout.DEFAULT_SIZE, 920, Short.MAX_VALUE)
+				.addComponent(scrollPane, GroupLayout.DEFAULT_SIZE, 940, Short.MAX_VALUE)
 		);
 		gl_contentPane.setVerticalGroup(
 			gl_contentPane.createParallelGroup(Alignment.LEADING)
@@ -198,37 +114,11 @@ public class Receipt extends JFrame {
 						.addComponent(lblNewLabel_4)
 						.addComponent(lblNewLabel_4_2, GroupLayout.PREFERRED_SIZE, 24, GroupLayout.PREFERRED_SIZE))
 					.addPreferredGap(ComponentPlacement.RELATED)
-					.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING)
-						.addGroup(gl_contentPane.createParallelGroup(Alignment.BASELINE)
-							.addComponent(lblNewLabel_1)
-							.addComponent(namedisplay))
-						.addGroup(gl_contentPane.createParallelGroup(Alignment.BASELINE)
-							.addComponent(orderdatedisplay, GroupLayout.PREFERRED_SIZE, 21, GroupLayout.PREFERRED_SIZE)
-							.addComponent(lblNewLabel_1_2, GroupLayout.PREFERRED_SIZE, 21, GroupLayout.PREFERRED_SIZE)))
-					.addGap(6)
 					.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING, false)
-						.addGroup(gl_contentPane.createSequentialGroup()
-							.addGroup(gl_contentPane.createParallelGroup(Alignment.TRAILING)
-								.addComponent(lblNewLabel_1_2_1, GroupLayout.PREFERRED_SIZE, 21, GroupLayout.PREFERRED_SIZE)
-								.addComponent(orderiddisplay, GroupLayout.PREFERRED_SIZE, 21, GroupLayout.PREFERRED_SIZE))
-							.addPreferredGap(ComponentPlacement.RELATED)
-							.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING)
-								.addComponent(lblNewLabel_1_2_2, GroupLayout.PREFERRED_SIZE, 21, GroupLayout.PREFERRED_SIZE)
-								.addComponent(paymenttypedisplay, GroupLayout.PREFERRED_SIZE, 21, GroupLayout.PREFERRED_SIZE)))
-						.addGroup(gl_contentPane.createSequentialGroup()
-							.addGroup(gl_contentPane.createParallelGroup(Alignment.BASELINE)
-								.addComponent(lblNewLabel_2)
-								.addComponent(phonenodisplay))
-							.addPreferredGap(ComponentPlacement.RELATED)
-							.addGroup(gl_contentPane.createParallelGroup(Alignment.BASELINE)
-								.addComponent(lblNewLabel_2_2, GroupLayout.PREFERRED_SIZE, 21, GroupLayout.PREFERRED_SIZE)
-								.addComponent(lblGendeDisp, GroupLayout.PREFERRED_SIZE, 21, GroupLayout.PREFERRED_SIZE))
-							.addPreferredGap(ComponentPlacement.RELATED)
-							.addGroup(gl_contentPane.createParallelGroup(Alignment.BASELINE)
-								.addComponent(lblNewLabel_2_1, GroupLayout.PREFERRED_SIZE, 21, GroupLayout.PREFERRED_SIZE)
-								.addComponent(addressdisplay, GroupLayout.PREFERRED_SIZE, 65, GroupLayout.PREFERRED_SIZE))))
-					.addPreferredGap(ComponentPlacement.UNRELATED)
-					.addComponent(scrollPane, GroupLayout.DEFAULT_SIZE, 240, Short.MAX_VALUE)
+						.addComponent(panel_1, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+						.addComponent(panel_2, 0, 0, Short.MAX_VALUE))
+					.addGap(18)
+					.addComponent(scrollPane, GroupLayout.DEFAULT_SIZE, 246, Short.MAX_VALUE)
 					.addPreferredGap(ComponentPlacement.RELATED)
 					.addComponent(lblNewLabel_4_1, GroupLayout.PREFERRED_SIZE, 26, GroupLayout.PREFERRED_SIZE)
 					.addPreferredGap(ComponentPlacement.RELATED)
@@ -237,6 +127,149 @@ public class Receipt extends JFrame {
 					.addComponent(balancedisp, GroupLayout.PREFERRED_SIZE, 15, GroupLayout.PREFERRED_SIZE)
 					.addContainerGap())
 		);
+		
+		JLabel lblNewLabel_1_2_2 = new JLabel("Payment Type:");
+		lblNewLabel_1_2_2.setForeground(Color.BLACK);
+		lblNewLabel_1_2_2.setFont(new Font("SansSerif", Font.PLAIN, 16));
+		
+		JLabel lblNewLabel_1_2 = new JLabel("Order Date:");
+		lblNewLabel_1_2.setForeground(Color.BLACK);
+		lblNewLabel_1_2.setFont(new Font("SansSerif", Font.PLAIN, 16));
+		
+		JLabel orderdatedisplay = new JLabel(orderdata.get(0).getdate() + " " + orderdata.get(0).getordertime());
+		orderdatedisplay.setHorizontalAlignment(SwingConstants.RIGHT);
+		orderdatedisplay.setForeground(Color.BLACK);
+		orderdatedisplay.setFont(new Font("SansSerif", Font.PLAIN, 14));
+		
+		JLabel lblNewLabel_1_2_1 = new JLabel("Order ID:");
+		lblNewLabel_1_2_1.setForeground(Color.BLACK);
+		lblNewLabel_1_2_1.setFont(new Font("SansSerif", Font.PLAIN, 16));
+		
+		JLabel orderiddisplay = new JLabel(orderid);
+		orderiddisplay.setHorizontalAlignment(SwingConstants.RIGHT);
+		orderiddisplay.setForeground(Color.BLACK);
+		orderiddisplay.setFont(new Font("SansSerif", Font.PLAIN, 14));
+		
+		JLabel paymenttypedisplay = new JLabel(paymentdata.get(0).getpaymenttype());
+		paymenttypedisplay.setHorizontalAlignment(SwingConstants.RIGHT);
+		paymenttypedisplay.setForeground(Color.BLACK);
+		paymenttypedisplay.setFont(new Font("SansSerif", Font.PLAIN, 14));
+		GroupLayout gl_panel_2 = new GroupLayout(panel_2);
+		gl_panel_2.setHorizontalGroup(
+			gl_panel_2.createParallelGroup(Alignment.LEADING)
+				.addGroup(gl_panel_2.createSequentialGroup()
+					.addContainerGap()
+					.addGroup(gl_panel_2.createParallelGroup(Alignment.LEADING)
+						.addGroup(Alignment.TRAILING, gl_panel_2.createSequentialGroup()
+							.addComponent(lblNewLabel_1_2)
+							.addPreferredGap(ComponentPlacement.RELATED, 51, Short.MAX_VALUE)
+							.addComponent(orderdatedisplay, GroupLayout.PREFERRED_SIZE, 209, GroupLayout.PREFERRED_SIZE))
+						.addGroup(gl_panel_2.createSequentialGroup()
+							.addComponent(lblNewLabel_1_2_1, GroupLayout.PREFERRED_SIZE, 82, GroupLayout.PREFERRED_SIZE)
+							.addPreferredGap(ComponentPlacement.RELATED, 49, Short.MAX_VALUE)
+							.addComponent(orderiddisplay, GroupLayout.PREFERRED_SIZE, 211, GroupLayout.PREFERRED_SIZE))
+						.addGroup(Alignment.TRAILING, gl_panel_2.createSequentialGroup()
+							.addComponent(lblNewLabel_1_2_2, GroupLayout.PREFERRED_SIZE, 113, GroupLayout.PREFERRED_SIZE)
+							.addPreferredGap(ComponentPlacement.RELATED, 133, Short.MAX_VALUE)
+							.addComponent(paymenttypedisplay, GroupLayout.PREFERRED_SIZE, 96, GroupLayout.PREFERRED_SIZE)))
+					.addContainerGap())
+		);
+		gl_panel_2.setVerticalGroup(
+			gl_panel_2.createParallelGroup(Alignment.LEADING)
+				.addGroup(gl_panel_2.createSequentialGroup()
+					.addContainerGap()
+					.addGroup(gl_panel_2.createParallelGroup(Alignment.BASELINE)
+						.addComponent(orderdatedisplay, GroupLayout.PREFERRED_SIZE, 21, GroupLayout.PREFERRED_SIZE)
+						.addComponent(lblNewLabel_1_2))
+					.addPreferredGap(ComponentPlacement.RELATED)
+					.addGroup(gl_panel_2.createParallelGroup(Alignment.BASELINE)
+						.addComponent(lblNewLabel_1_2_1)
+						.addComponent(orderiddisplay, GroupLayout.PREFERRED_SIZE, 21, GroupLayout.PREFERRED_SIZE))
+					.addPreferredGap(ComponentPlacement.UNRELATED)
+					.addGroup(gl_panel_2.createParallelGroup(Alignment.BASELINE)
+						.addComponent(paymenttypedisplay, GroupLayout.PREFERRED_SIZE, 21, GroupLayout.PREFERRED_SIZE)
+						.addComponent(lblNewLabel_1_2_2))
+					.addContainerGap(100, Short.MAX_VALUE))
+		);
+		panel_2.setLayout(gl_panel_2);
+		
+		JLabel lblNewLabel_2_1 = new JLabel("Address:");
+		lblNewLabel_2_1.setForeground(Color.BLACK);
+		lblNewLabel_2_1.setFont(new Font("SansSerif", Font.PLAIN, 16));
+		JLabel addressdisplay = new JLabel(addressline);
+		addressdisplay.setVerticalAlignment(SwingConstants.TOP);
+		addressdisplay.setForeground(Color.BLACK);
+		addressdisplay.setFont(new Font("SansSerif", Font.PLAIN, 14));
+		
+		JLabel lblNewLabel_2_2 = new JLabel("Gender:");
+		lblNewLabel_2_2.setForeground(Color.BLACK);
+		lblNewLabel_2_2.setFont(new Font("SansSerif", Font.PLAIN, 16));
+		
+		JLabel lblGendeDisp = new JLabel(customerdata.get(0).getgender());
+		lblGendeDisp.setForeground(Color.BLACK);
+		lblGendeDisp.setFont(new Font("SansSerif", Font.PLAIN, 14));
+		
+				JLabel lblNewLabel_2 = new JLabel("Phone Number:");
+				lblNewLabel_2.setForeground(Color.BLACK);
+				lblNewLabel_2.setFont(new Font("SansSerif", Font.PLAIN, 16));
+		
+		JLabel phonenodisplay = new JLabel(customerdata.get(0).getphoneno());
+		phonenodisplay.setForeground(Color.BLACK);
+		phonenodisplay.setFont(new Font("SansSerif", Font.PLAIN, 14));
+		
+				JLabel lblNewLabel_1 = new JLabel("Name:");
+				lblNewLabel_1.setForeground(Color.BLACK);
+				lblNewLabel_1.setFont(new Font("SansSerif", Font.PLAIN, 16));
+		
+		JLabel namedisplay = new JLabel(customerdata.get(0).getname());
+		namedisplay.setForeground(Color.BLACK);
+		namedisplay.setFont(new Font("SansSerif", Font.PLAIN, 14));
+		GroupLayout gl_panel_1 = new GroupLayout(panel_1);
+		gl_panel_1.setHorizontalGroup(
+			gl_panel_1.createParallelGroup(Alignment.LEADING)
+				.addGroup(gl_panel_1.createSequentialGroup()
+					.addContainerGap()
+					.addGroup(gl_panel_1.createParallelGroup(Alignment.LEADING)
+						.addGroup(Alignment.TRAILING, gl_panel_1.createSequentialGroup()
+							.addComponent(lblNewLabel_2_1, GroupLayout.DEFAULT_SIZE, 73, Short.MAX_VALUE)
+							.addPreferredGap(ComponentPlacement.RELATED)
+							.addComponent(addressdisplay, GroupLayout.PREFERRED_SIZE, 455, GroupLayout.PREFERRED_SIZE))
+						.addGroup(gl_panel_1.createSequentialGroup()
+							.addComponent(lblNewLabel_2_2)
+							.addGap(18)
+							.addComponent(lblGendeDisp, GroupLayout.PREFERRED_SIZE, 207, GroupLayout.PREFERRED_SIZE))
+						.addGroup(gl_panel_1.createSequentialGroup()
+							.addComponent(lblNewLabel_2)
+							.addPreferredGap(ComponentPlacement.UNRELATED)
+							.addComponent(phonenodisplay, GroupLayout.PREFERRED_SIZE, 207, GroupLayout.PREFERRED_SIZE))
+						.addGroup(gl_panel_1.createSequentialGroup()
+							.addComponent(lblNewLabel_1)
+							.addPreferredGap(ComponentPlacement.UNRELATED)
+							.addComponent(namedisplay, GroupLayout.PREFERRED_SIZE, 333, GroupLayout.PREFERRED_SIZE)))
+					.addContainerGap())
+		);
+		gl_panel_1.setVerticalGroup(
+			gl_panel_1.createParallelGroup(Alignment.LEADING)
+				.addGroup(gl_panel_1.createSequentialGroup()
+					.addGap(12)
+					.addGroup(gl_panel_1.createParallelGroup(Alignment.BASELINE)
+						.addComponent(lblNewLabel_1)
+						.addComponent(namedisplay))
+					.addPreferredGap(ComponentPlacement.RELATED)
+					.addGroup(gl_panel_1.createParallelGroup(Alignment.BASELINE)
+						.addComponent(lblNewLabel_2)
+						.addComponent(phonenodisplay))
+					.addPreferredGap(ComponentPlacement.RELATED)
+					.addGroup(gl_panel_1.createParallelGroup(Alignment.TRAILING)
+						.addComponent(lblGendeDisp, GroupLayout.PREFERRED_SIZE, 21, GroupLayout.PREFERRED_SIZE)
+						.addComponent(lblNewLabel_2_2))
+					.addPreferredGap(ComponentPlacement.RELATED)
+					.addGroup(gl_panel_1.createParallelGroup(Alignment.LEADING)
+						.addComponent(addressdisplay, GroupLayout.PREFERRED_SIZE, 70, GroupLayout.PREFERRED_SIZE)
+						.addComponent(lblNewLabel_2_1))
+					.addContainerGap(10, Short.MAX_VALUE))
+		);
+		panel_1.setLayout(gl_panel_1);
 		
 		table = new JTable();
 		DefaultTableModel listitemmodel = new DefaultTableModel(
